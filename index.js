5000a8a31b60baf945f4e4f964919c97ec152d9b
Shabex <shabsibrah@gmail.com>
2022-06-03 23:46:28 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.querySelector ("main");
+main.remove();
+
+const newHeader = document.createElement('H1');
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "<h1>YOUR-NAME is the champion<\h1>"
+element.append (h1);
+
+
+
+
